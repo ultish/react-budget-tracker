56ab93c3cfbede87c05e02fdace15790cd81f10b
Jimmy <ultish@gmail.com>
2021-03-21 16:13:14 +1100

    my

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
     return (
         <AppProvider>
             <div className="container">
-                <h1 className="mt-3">Mu Budget Planner</h1>
+                <h1 className="mt-3">My Budget Planner</h1>
                 <div className="row mt-3">
                     <div className="col-sm">
                         <Budget/>

dc661f4fe8d1726f25c6c87a3e21b38e28c57da1
Jimmy <ultish@gmail.com>
2021-03-21 16:08:35 +1100

    add filter expenses

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,24 +1,47 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import ExpenseItem from './ExpenseItem';
 import {AppContext} from "../context/AppContext";
 
 const ExpenseList = () => {
     const {state: {expenses}} = useContext(AppContext);
+
+    const [filter, setFilter] = useState('');
+
+    const filterUpper = filter?.toUpperCase();
+
+    const filteredList = expenses.filter(exp => {
+        if (filter && filter.length) {
+            return exp.name.toUpperCase().includes(filterUpper);
+        } else {
+            return true
+        }
+    })
+
     return (
-        <ul className="list-group">
-            {
-                expenses.map(expense => {
-                    return (
-                        <ExpenseItem
-                            key={expense.id}
-                            id={expense.id}
-                            name={expense.name}
-                            cost={expense.cost}
-                        />
-                    )
-                })
-            }
-        </ul>
+        <>
+
+            <input
+                placeholder='Filter...'
+                type="text"
+                className="form-control mb-3"
+                value={filter}
+                onChange={e => setFilter(e.target.value)}
+            />
+            <ul className="list-group">
+                {
+                    filteredList.map(expense => {
+                        return (
+                            <ExpenseItem
+                                key={expense.id}
+                                id={expense.id}
+                                name={expense.name}
+                                cost={expense.cost}
+                            />
+                        )
+                    })
+                }
+            </ul>
+        </>
     )
 }
 export default ExpenseList;
